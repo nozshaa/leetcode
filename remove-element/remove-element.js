f4b4c27a64e91a418290be00cf851bc00ef19d37
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-05-23 15:21:22 +0200

    Time: 72 ms (95.38%), Space: 38.5 MB (69.26%) - LeetHub

--- remove-element/remove-element.js
+++ remove-element/remove-element.js
@@ -4,13 +4,16 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    // slow runner
     let i = 0;
-    
-    for(let j = 0; j < nums.length; j++) {
+    // go to loop with fast runner
+    for(let j = 0; j < nums.length; j ++) {
+        // skip the val
         if(nums[j] !== val) {
             nums[i] = nums[j];
             i ++;
         }
     }
+    // because we first ++ and then break loop
     return i;
 };
