a367d4f218325107a27b2c6713c0fad94bc79f7f
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-05-31 15:04:14 +0200

    Time: 724 ms (5.05%), Space: 46.2 MB (5.11%) - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -2,43 +2,42 @@
  * @param {number} n
  * @return {number}
  */
-// var fib = function(n, memo = new Map()) {
-//     if (n === 0) {
-//         return 0;
-//     }
-//     if (n === 1) {
-//         return 1;
-//     }
-//     if (memo.has(n)) {
-//         return memo.get(n);
-//     }
-//     let res = fib(n-1, memo) + fib(n-2, memo);
-//     memo.set(n, res);
-//     return res;
-//     // T.C: O(N)
-//     // S.C: O(N)
-// };
 var fib = function(n) {
-    let memoize = memoizeFib();
-    return memoize(n);
+    return fibonacci(n, memo = new Map());
 };
-var memoizeFib = function() {
-    const cache = {};
-    var fibo = function(n) {
-        if(n < 2) {
-            return n;
-        }
-        const result = fibo(n - 1) + fibo(n - 2);
-        cache[n] = result;
-        return result;
+var fibonacci = function(n , memo = new Map()) {
+    if (n < 2) {
+        return n;
     }
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    let res = fib(n-1, memo) + fib(n-2, memo);
+    memo.set(n, res);
+    return res;
+};
+//with closure
+// var fib = function(n) {
+//     let memoize = memoizeFib();
+//     return memoize(n);
+// };
+// var memoizeFib = function() {
+//     const cache = {};
+//     var fibo = function(n) {
+//         if(n < 2) {
+//             return n;
+//         }
+//         const result = fibo(n - 1) + fibo(n - 2);
+//         cache[n] = result;
+//         return result;
+//     }
     
-    return (n) => {
-        if(cache[n]) {
-            return cache[n];
-        } else {
-            const result = fibo(n);
-            return result;
-        }
-    }
-}
+//     return (n) => {
+//         if(cache[n]) {
+//             return cache[n];
+//         } else {
+//             const result = fibo(n);
+//             return result;
+//         }
+//     }
+// }
