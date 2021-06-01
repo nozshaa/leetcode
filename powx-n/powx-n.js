7a43da92cb4e4059337586ecdbb5cb904728e0ff
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-06-01 14:10:11 +0200

    Time: 88 ms (32.12%), Space: 38.7 MB (80.02%) - LeetHub

--- powx-n/powx-n.js
+++ powx-n/powx-n.js
@@ -3,18 +3,24 @@
  * @param {number} n
  * @return {number}
  */
-// var myPow = function(x, n) {
-//     if(n === 0) {
-//         return 1;
-//     }
-//     return n%2===0 ? myPow(x*x, n/2) : x*myPow(x, n-1);
-// };
-var myPow = function(x, n) {
-    if (n===0) return 1;
-    
-    let pow = Math.abs(n);
-    
-	let result = pow%2===0 ? myPow(x*x,pow/2) : myPow(x*x,(pow-1)/2) * x;
-    
-    return n < 0 ? 1/result : result;
-};
+ const cache = {};
+const fastPow = (x, n) => {
+    if (n == 0) {
+        return 1;
+    }
+    const number = Math.floor(n/2);
+    const half = fastPow(x, number);
+    if (n % 2 == 0) {
+        return half * half;
+    } else {
+        return half * half * x;
+    }
+}
+ var myPow = function(x, n) {
+     if (n < 0) {
+         x = 1/x;
+         n = -n;
+     }
+     return fastPow(x, n);
+ };
+ 
