909cd92afd60eca5964db7c35548d0acbb17866e
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-06-02 18:55:53 +0200

    Time: 100 ms (6.73%), Space: 38.9 MB (28.72%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,28 +3,35 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let left=0;
-    let right=nums.length-1;
+var search = function(nums, target) { 
     
-    while(left<=right){
-        const mid=Math.floor((right+left)/2);
+    let start = 0;
+    let end = nums.length -1;
+    let mid = 0;
+    
+    while (start <= end) {
+        mid = Math.floor(start + (end - start) / 2);
         
-        if(target===nums[mid]){
-            return mid;
-        } else if(nums[mid]>=nums[left]){
-
-            if(target >= nums[left] && target<nums[mid]) {
-                right=mid-1;
-            } else{
-                left=mid+1;
+        if (nums[start] === target) return start;
+        if (nums[end] === target) return end;
+        if (nums[mid] === target) return mid;
+        
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] < target && target < nums[mid]) {
+                end = mid - 1;    
+            } else {
+                start = mid + 1;    
             }
-        } else if(target <= nums[right] && target>nums[mid]) {
-            left=mid+1;
-        } else{
-            right=mid-1
+            
+        } else {
+            if (nums[mid] < target && target < nums[end]) {
+                start = mid + 1;  
+            } else {
+                end = mid - 1;
+            }            
         }
         
     }
+    
     return -1;
 };
