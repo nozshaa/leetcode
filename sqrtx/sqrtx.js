100c4a2389369378a074843add2961e3569d7e0e
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-06-02 15:23:00 +0200

    Time: 104 ms (37.56%), Space: 40.1 MB (22.98%) - LeetHub

--- sqrtx/sqrtx.js
+++ sqrtx/sqrtx.js
@@ -4,19 +4,20 @@
  */
 var mySqrt = function(x) {
         // Base cases
-    if (x == 0 || x == 1)
-        return x;
- 
-    // Staring from 1, try all
-    // numbers until i*i is
-    // greater than or equal to x.
-    let i = 1;
-    let result = 1;
-     
-    while (result <= x)
-    {
-        i++;
-        result = i * i;
+    if(x<2) return x;
+    let l=2;
+    let h=Math.floor(x/2);
+    
+    while(l<=h){
+        const m=Math.floor((l+h)/2);
+        const num=m*m;
+        if(num===x){
+            return m;
+        } else if(num<x){
+            l=m+1;
+        } else{
+            h=m-1;
+        }
     }
-    return i - 1;
+    return h;
 };
