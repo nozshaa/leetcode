59289fde84206d5e71af73168a8478db71a594f3
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-06-18 09:49:46 +0200

    Time: 116 ms (67.74%), Space: 42.4 MB (57.41%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -4,19 +4,17 @@
  */
 var firstUniqChar = function(s) {
     const string = s;
-    let countingCharacters = {};
+	let countingCharacters = {};
 	for(const letter of string) {
-		if(!(letter in countingCharacters))
-			countingCharacters[letter] = 0;
+		if(!(letter in countingCharacters)) countingCharacters[letter] = 0;
 		countingCharacters[letter]++;
 	}
-	console.log(countingCharacters);
+
 	for(let i = 0; i < string.length; i++) {
 		const letter = string[i];
 		// (letter in countingCharacters) && countingCharacters[letter] === 1
-		if(countingCharacters[letter] === 1) {
-			return i;
-		}
+		if(countingCharacters[letter] === 1) return i;
 	}
+	
   return -1;
 };
