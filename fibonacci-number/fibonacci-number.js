cd75076bea3259abee1805f5ce25f0a507028622
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-06-21 10:07:41 +0200

    Time: 6508 ms (5.00%), Space: 45.4 MB (5.17%) - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -3,41 +3,11 @@
  * @return {number}
  */
 var fib = function(n) {
-    return fibonacci(n, memo = new Map());
+   	if(n < 2) return n;
+	
+	const x = fib(n - 2);
+	const y = fib(n-1);
+	const z = x+y;
+	console.log(x, y)
+	return x+y;
 };
-var fibonacci = function(n , memo = new Map()) {
-    if (n < 2) {
-        return n;
-    }
-    if (memo.has(n)) {
-        return memo.get(n);
-    }
-    let res = fib(n-1, memo) + fib(n-2, memo);
-    memo.set(n, res);
-    return res;
-};
-//with closure
-// var fib = function(n) {
-//     let memoize = memoizeFib();
-//     return memoize(n);
-// };
-// var memoizeFib = function() {
-//     const cache = {};
-//     var fibo = function(n) {
-//         if(n < 2) {
-//             return n;
-//         }
-//         const result = fibo(n - 1) + fibo(n - 2);
-//         cache[n] = result;
-//         return result;
-//     }
-    
-//     return (n) => {
-//         if(cache[n]) {
-//             return cache[n];
-//         } else {
-//             const result = fibo(n);
-//             return result;
-//         }
-//     }
-// }
