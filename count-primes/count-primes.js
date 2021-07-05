4a7172d2d020029efe5041c2bee807b912b3885e
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-07-05 08:57:19 +0200

    Time: 340 ms (50.28%), Space: 112 MB (34.07%) - LeetHub

--- count-primes/count-primes.js
+++ count-primes/count-primes.js
@@ -2,38 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-
-
-
-const countPrimes = (n) => {
-  if (n <= 1) {
-    return []
-  }
-
-  const output = [];
-  //Declare a Boolean Array for storing if a number is prime or not
-  const isPrime = Array(n).fill(false)
-
-  // Initialise every element of this array as true
-  for (let i = 0; i < n; i++) {
-    isPrime[i] = true;
-  }
-  
-  // Since 0 and 1 are not prime we declare them false explicitly
-  isPrime[0] = false;
-  isPrime[1] = false;
-
-  for (let i = 0; i < n; i++) {
-    // Only check if isPrime[i]==true
-    if (isPrime[i]) {
-       // Mark all the factor of (i) as Not Prime
-      for (let j = i + i; j < n; j += i) {
-        isPrime[j] = false;
-      }
-      //Accumulating all the prime numbers in out ans list
-      output.push(i);
+var countPrimes = function(n) {
+    const primes = Array(n).fill(true);
+    for(let i = 0; i < n; i++) {
+        primes[0] = false;
+        primes[1] = false;
+        
+        if(primes[i]) {
+            for(let j = 2; i*j < n; j++) {
+                primes[i*j] = false;
+            }
+        }
     }
-  }
-
-  return output.length;
-}
+    console.log(primes)
+    return primes.filter(prime => prime === true).length
+    
+};
