f32eaa89a50e5c15239911bc80229ad85d22d5a8
Nastaran <84511621+nozshaa@users.noreply.github.com>
2021-07-10 20:53:19 +0200

    Time: 80 ms (76.47%), Space: 40.7 MB (10.34%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -5,10 +5,12 @@
 var maxSubArray = function(nums) {
     let maxEndigHere = nums[0];
     let maxSofar = nums[0];
+    let result = []
     
     for(let idx = 1; idx < nums.length; idx++) {
         const num = nums[idx];
         maxEndigHere = Math.max(num, maxEndigHere + num);
+        result.push(maxEndigHere)
         maxSofar = Math.max(maxEndigHere, maxSofar);
     }
     return maxSofar;
